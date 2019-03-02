0e84a00bc8b05470b251d813dbcacb3b519f16d2
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-03-02 17:06:15 +0100

    Showing countryCode

--- src/main/java/com/awesome2048/score/Score.java
+++ src/main/java/com/awesome2048/score/Score.java
@@ -46,7 +46,7 @@ public class Score {
     public String getCountryCode() {
         String endpoint = "http://api.ipinfodb.com/v3/ip-country/?key=62ee2a10303261af0cf55d6eb2c807c8db5e6fa539fe5ba843c341f4062bfaea&ip= " + this.getIp();
         RestTemplate restTemplate = new RestTemplate();
-        String countryCode = restTemplate.getForObject(endpoint, String.class).split(";")[2];
+        String countryCode = restTemplate.getForObject(endpoint, String.class).split(";")[3];
         return countryCode;
     }
 }
