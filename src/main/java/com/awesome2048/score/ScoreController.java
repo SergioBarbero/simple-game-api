5114f32870e0f540b96186a2af2dd63c9c461ee6
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-03-02 17:04:55 +0100

    Removed unused imports

--- src/main/java/com/awesome2048/score/ScoreController.java
+++ src/main/java/com/awesome2048/score/ScoreController.java
@@ -1,15 +1,11 @@
 package com.awesome2048.score;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.awesome2048.score.ScoreRepository;
-
 import java.util.List;
 
 @RestController
