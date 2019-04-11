4aca476dd16d44de0fa1d26d6a17b7f3108d3a7a
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-04-11 22:37:24 +0200

    Reading inside

--- src/main/java/com/awesome2048/score/ExternalService.java
+++ src/main/java/com/awesome2048/score/ExternalService.java
@@ -3,13 +3,15 @@ package com.awesome2048.score;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 public class ExternalService {
 
-    public static String readField(String json, String name) throws IOException {
+    public static String readFieldFromGetRequest(String query, String name) throws IOException {
         if (name != null) {
+            String json = new RestTemplate().getForObject(query, String.class);
             ObjectNode object = new ObjectMapper().readValue(json, ObjectNode.class);
             JsonNode node = object.get(name);
             return (node == null ? null : node.textValue());
--- src/main/java/com/awesome2048/score/ScoreController.java
+++ src/main/java/com/awesome2048/score/ScoreController.java
@@ -24,8 +24,7 @@ public class ScoreController {
     @PostMapping("/scores")
     public Score postScore(@RequestBody Score newScore, HttpServletRequest request) throws IOException {
         String query = "http://api.ipinfodb.com/v3/ip-country/?key=62ee2a10303261af0cf55d6eb2c807c8db5e6fa539fe5ba843c341f4062bfaea&format=json&ip=" + request.getRemoteAddr();
-        String userInfo = new RestTemplate().getForObject(query, String.class);
-        String countryCode = ExternalService.readField(userInfo, "countryCode");
+        String countryCode = ExternalService.readFieldFromGetRequest(query, "countryCode");
         newScore.setCountryCode(countryCode);
         repository.save(newScore);
         return newScore;
