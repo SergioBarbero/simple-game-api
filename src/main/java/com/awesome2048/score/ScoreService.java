d4d9d3ed6086734089f71761726af38d533923bb
Sergio Barbero <sergio.barbero.bascones95@gmail.com>
2019-08-16 20:43:45 +0200

    Anotated service

--- src/main/java/com/awesome2048/score/ScoreService.java
+++ src/main/java/com/awesome2048/score/ScoreService.java
@@ -1,9 +1,11 @@
 package com.awesome2048.score;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ScoreService {
 
     @Autowired
